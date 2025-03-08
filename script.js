13706068bc76b2c9f59e6e5f1ba112ff3911f0c0
Biplop-Rai <rai.biplop30@gmail.com>
2025-03-08 22:17:37 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello no yesy yes, hello  hello");
+console.log("Hello no yesy yes, no nono ");
+ 
+// Variable means a container that stores data.
+// for example:
+let cup = "coffee";
+console.log(cup);
+
+cup = "water";
+console.log(cup);// this is string value which is denoted by "".
+
+gamala = "flower";
+console.log(gamala);
+
+// you can use let or var to declare a variable.
+let name = "John";
+console.log(name);
+
+// constant means  you can not change the value of the variable.
+// for example:
+const pi = 3.14;
+console.log(pi);
+const gravity = 9.8;// (this value is integer or number)
+console.log(gravity);
+
+var password = "123456"; // this allows you to change the value of the variable. 
+// but if you used constants instead of var,
+//  you can not change the value of the variable.
+// therefore you wouldn't have to face problem in password.
+
+isNepali = true;//(this is boolean value which is denoted by true or false).
+// primitive data types are: string, number, boolean, null, undefined.
+// non-primitive data types are: object, array, function.
+
+var password = "haha";
+
+console.log("Mero gamala ma k xa vai?"+gamala + "xa vai");
+console.log(`Mero gamala ma k xa vai?" ${gamala}" xa vai`);
+let age = 20;
+console.log(`My age is ${age}`);
+console.log("My age is " + age);
+
