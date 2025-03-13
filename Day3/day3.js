c3cbf2bbe8628e0409705f6278c97157cc16684d
Biplop-Rai <rai.biplop30@gmail.com>
2025-03-13 20:53:43 +0545

    Day 3 commit Successfull

---
+++ Day3/day3.js
@@ -0,0 +1,76 @@
+//Variable
+let juta1 = "goldstar";
+let juta2 = "adidas";
+let juta3 = "nike";0
+
+// array
+
+let jutas = ["goldstar", "adidas", "nike"];
+console.log(jutas);
+
+//variable
+
+let country1 = "Nepal";
+let country2 = "India";
+let country3 = "Pakistan";
+
+//array
+
+let countries = ["Nepal", "India", "Pakistan"];
+console.log(countries);
+
+//variable
+let friend1 = "Ram";
+let friend2 = "Shyam";
+
+//array
+let friends = ["Ram", "Shyam"];
+console.log(friends);
+
+let name1 = "Biplop Rai ";
+let address = "Kathmandu";
+let age = "20";
+let college = "ISMT College";
+  
+//array methods
+// jutas.push("puma"); //add element at the end of the array
+// jutas.unshift("puma"); //add element at the beginning of the array
+// jutas.pop(); //delete element at the end of the array
+// jutas.shift(); //delete element at the beginning of the array
+//jutsa[1] = "puma"; //replace the element at the index 1
+// console.log(jutas); // print the array
+console.log(jutas[0]); // print the first element of the array as array index starts from 0.
+
+//object
+let myInformation = {
+    name: "Biplop Rai",
+    address: "Kathmandu",
+    age: "20",
+    college: "ISMT College"};
+
+console.log(myInformation);
+
+//object methods
+
+
+// Case or Naming Convention
+// 1. camelCase
+// Example: iloveProgramming --> camelCase --> iLoveProgramming
+// example: iamfromnepal --> camelCase --> iAmFromNepal
+
+
+// 2. snake_case
+// Example: iloveprogramming --> snake_case --> i_love_programming
+// example: iamfromnepal --> snake_case --> i_am_from_nepal
+
+
+// 3. PascalCase
+// Example: iloveprogramming --> PascalCase --> ILoveProgramming
+// example: iamfromnepal --> PascalCase --> IAmFromNepal
+
+
+// 4. kebab-case
+// Example: iloveprogramming --> kebab-case --> i-love-programming
+// example: iamfromnepal --> kebab-case --> i-am-from-nepal
+
+
